0847f899b1ad64744cad852aa311d277f65348a7
Dat Pham <phamtiendat.dev@gmail.com>
2024-07-16 18:26:43 +0700

    fix: limit payload

--- index.js
+++ index.js
@@ -46,12 +46,8 @@ app.use(express.json());
 app.use(morgan("dev"));
 // Cấu hình parser
 app.use(cookieParser());
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({
-    limit: '50mb',
-    extended: true,
-    parameterLimit: 50000
-}));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
 // Database
 // Database - Connect
 const Room = require('./models/room');
